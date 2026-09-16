445eb264fc50c182bda52bc846d204f5de5c2d94
b1o <erikghabuzyan6@gmail.com>
2026-09-15 19:36:17 -0700

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/forest/',
   build: { target: 'es2020', cssMinify: 'lightningcss' }
 })
